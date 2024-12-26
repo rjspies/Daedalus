5897d00be0b44615ce9445de23de7d8e7c0fabd1
René Jörg Spies <24745517-rjspies@users.noreply.gitlab.com>
2024-12-26 23:30:28 +0000

    Raise kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.comAndroidApplication) apply false
-    alias(libs.plugins.comAndroidLibrary) apply false
     alias(libs.plugins.orgJetbrainsKotlinAndroid) apply false
     alias(libs.plugins.orgJmailenKotlinter) apply false
     alias(libs.plugins.ioGitlabArturboschDetekt) apply false
