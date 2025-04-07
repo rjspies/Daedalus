8f1a90142e1a1391e9a50a7caa9c0fe523337cc2
René Jörg Spies <121790537+rjspies@users.noreply.github.com>
2025-04-08 01:01:13 +0200

    Fix ktlint

--- app/src/main/kotlin/com/rjspies/daedalus/IntentActions.kt
+++ app/src/main/kotlin/com/rjspies/daedalus/IntentActions.kt
@@ -2,6 +2,8 @@ package com.rjspies.daedalus
 
 private const val PREFIX = "com.rjspies.daedalus.intent.action"
 
-sealed class IntentActions(val action: String) {
+sealed class IntentActions(
+    val action: String,
+) {
     data object AddWeight : IntentActions("$PREFIX.ADD_WEIGHT")
 }
