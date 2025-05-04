8d4c996c5f24c53055a79163c8d63e6f63fc5079
René Jörg Spies <121790537+rjspies@users.noreply.github.com>
2025-05-04 16:40:01 +0200

    Remove settings page

--- app/src/main/kotlin/com/rjspies/daedalus/ui/settings/SettingsUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.rjspies.daedalus.ui.settings
-
-import com.rjspies.daedalus.ui.common.UiState
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class SettingsUiState(
-    val settingItems: List<SettingItem> = emptyList(),
-) : UiState
