6ab47ec71811a02982525113ecf199cb1a939e7b
René Jörg Spies <121790537+rjspies@users.noreply.github.com>
2025-05-04 16:51:57 +0200

    Use Adwaita Sans font

--- app/src/main/kotlin/com/rjspies/daedalus/ui/Typography.kt
+++ app/src/main/kotlin/com/rjspies/daedalus/ui/Typography.kt
@@ -7,74 +7,31 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import com.rjspies.daedalus.R
 
-val bodyFontFamily = FontFamily(
+private val fontFamily = FontFamily(
     Font(
-        resId = R.font.athiti_extra_light,
-        weight = FontWeight.ExtraLight,
-        style = FontStyle.Normal,
-    ),
-    Font(
-        resId = R.font.athiti_light,
-        weight = FontWeight.Light,
-        style = FontStyle.Normal,
-    ),
-    Font(
-        resId = R.font.athiti_regular,
+        resId = R.font.adwaita_sans_regular,
         weight = FontWeight.Normal,
         style = FontStyle.Normal,
-    ),
-    Font(
-        resId = R.font.athiti_medium,
-        weight = FontWeight.Medium,
-        style = FontStyle.Normal,
-    ),
-    Font(
-        resId = R.font.athiti_semi_bold,
-        weight = FontWeight.SemiBold,
-        style = FontStyle.Normal,
-    ),
-    Font(
-        resId = R.font.athiti_bold,
-        weight = FontWeight.Bold,
-        style = FontStyle.Normal,
-    ),
-)
-
-val displayFontFamily = FontFamily(
-    Font(
-        resId = R.font.cardo_regular,
-        weight = FontWeight.Normal,
-        style = FontStyle.Normal,
-    ),
-    Font(
-        resId = R.font.cardo_bold,
-        weight = FontWeight.Bold,
-        style = FontStyle.Normal,
-    ),
-    Font(
-        resId = R.font.cardo_italic,
-        weight = FontWeight.Normal,
-        style = FontStyle.Italic,
     ),
 )
 
 // Default Material 3 typography values
-val baseline = Typography()
+private val baseline = Typography()
 
 val DaedalusTypography = Typography(
-    displayLarge = baseline.displayLarge.copy(fontFamily = displayFontFamily),
-    displayMedium = baseline.displayMedium.copy(fontFamily = displayFontFamily),
-    displaySmall = baseline.displaySmall.copy(fontFamily = displayFontFamily),
-    headlineLarge = baseline.headlineLarge.copy(fontFamily = displayFontFamily),
-    headlineMedium = baseline.headlineMedium.copy(fontFamily = displayFontFamily),
-    headlineSmall = baseline.headlineSmall.copy(fontFamily = displayFontFamily),
-    titleLarge = baseline.titleLarge.copy(fontFamily = displayFontFamily),
-    titleMedium = baseline.titleMedium.copy(fontFamily = displayFontFamily),
-    titleSmall = baseline.titleSmall.copy(fontFamily = displayFontFamily),
-    bodyLarge = baseline.bodyLarge.copy(fontFamily = bodyFontFamily),
-    bodyMedium = baseline.bodyMedium.copy(fontFamily = bodyFontFamily),
-    bodySmall = baseline.bodySmall.copy(fontFamily = bodyFontFamily),
-    labelLarge = baseline.labelLarge.copy(fontFamily = bodyFontFamily),
-    labelMedium = baseline.labelMedium.copy(fontFamily = bodyFontFamily),
-    labelSmall = baseline.labelSmall.copy(fontFamily = bodyFontFamily),
+    displayLarge = baseline.displayLarge.copy(fontFamily = fontFamily),
+    displayMedium = baseline.displayMedium.copy(fontFamily = fontFamily),
+    displaySmall = baseline.displaySmall.copy(fontFamily = fontFamily),
+    headlineLarge = baseline.headlineLarge.copy(fontFamily = fontFamily),
+    headlineMedium = baseline.headlineMedium.copy(fontFamily = fontFamily),
+    headlineSmall = baseline.headlineSmall.copy(fontFamily = fontFamily),
+    titleLarge = baseline.titleLarge.copy(fontFamily = fontFamily),
+    titleMedium = baseline.titleMedium.copy(fontFamily = fontFamily),
+    titleSmall = baseline.titleSmall.copy(fontFamily = fontFamily),
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = fontFamily),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = fontFamily),
+    bodySmall = baseline.bodySmall.copy(fontFamily = fontFamily),
+    labelLarge = baseline.labelLarge.copy(fontFamily = fontFamily),
+    labelMedium = baseline.labelMedium.copy(fontFamily = fontFamily),
+    labelSmall = baseline.labelSmall.copy(fontFamily = fontFamily),
 )
